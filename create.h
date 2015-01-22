90f1e5c1af50a5c26a0538285a00de13a5c8ea2d
snape3058 <alansnape@outlook.com>
2015-01-22 10:41:04 +0800

    new, create new file

---
+++ create.h
@@ -0,0 +1,12 @@
+#ifndef _CREATE_H
+#define _CREATE_H
+
+#include <stdio.h>
+#include <string.h>
+
+#include "structure.h"
+
+void create(char * pwd, char * cmdstr, sysStatus * pstatus);
+
+#endif
+
